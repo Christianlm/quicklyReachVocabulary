7859ad85e75783ca6ef5790305a68098c26f9577
chrislm <llajta2012@gmail.com>
2018-12-29 19:51:49 +0100

    Using nvdaSettingsPanel and scriptDecorator

--- addon/globalPlugins/qrvocabulary/__init__.py
+++ addon/globalPlugins/qrvocabulary/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-#Copyright (C)2017
+#Copyright (C)2017-2018
 # Released under GPL 2
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
@@ -16,14 +16,15 @@ import cPickle
 import config
 import wx
 import gui
-from gui.settingsDialogs import SettingsDialog
-from gui import guiHelper
+from gui import SettingsPanel, NVDASettingsDialog, guiHelper
 from logHandler import log
 import scriptHandler
 import configobj
-import validate
-from cStringIO import StringIO
+#from cStringIO import StringIO
 import glob
+from globalCommands import SCRCAT_TOOLS
+from scriptHandler import script
+
 addonHandler.initTranslation()
 
 ADDON_DIR = os.path.dirname(__file__)
@@ -47,6 +48,21 @@ def loadVocabulary():
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
+	def __init__(self):
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		NVDASettingsDialog.categoryClasses.append(vocabularySettingsPanel)
+
+	def terminate(self):
+		NVDASettingsDialog.categoryClasses.remove(vocabularySettingsPanel)
+
+
+	@script(
+		# Translators: Message presented in input help mode.
+		description=_("Announces the meaning of selected word."),
+		category = SCRCAT_TOOLS,
+		gesture="kb:NVDA+shift+f7"
+	)
+
 	def script_findMeaning(self, gesture):
 		obj=api.getFocusObject()
 		treeInterceptor=obj.treeInterceptor
@@ -68,36 +84,29 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			else:
 				# Translator: message when the searched word is not present in the dictionary.
 				ui.message(_("no match found for this word... "))
-	# Translators: message presented in input mode, when a keystroke of this addon script is pressed.
-	script_findMeaning.__doc__ = _("Announces the meaning of selected word.")
 
-	def onVocabularySettingsDialog(self, evt):
-		gui.mainFrame._popupSettingsDialog(vocabularySettingsDialog)
+	def onVocabularySettingsPanel(self, evt):
+		gui.mainFrame._popupSettingsDialog(NVDASettingsDialog, VocabularySettingsPanel)
 
-	def script_vocabularySettings(self, gesture):
-		wx.CallAfter(self.onVocabularySettingsDialog, None)
-	# Translators: Input help mode message for go to Vocabulary  dialog.
-	script_vocabularySettings.__doc__ = _("Shows the list of available vocabularies")
-
-	__gestures = {
-		"kb:NVDA+shift+f7": "findMeaning",
-		"kb:NVDA+shift+f8": "vocabularySettings",
-	}
-
-
-class vocabularySettingsDialog(gui.SettingsDialog):
+class vocabularySettingsPanel(SettingsPanel):
 	# Translators: Title of the vocabulary settings dialog.
 	title = _("Vocabulary Settings")
 	def makeSettings(self, settingsSizer):
-		vocabularySettingsGuiHelper = gui.guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
 		# Translators: The label for  list in the vocabulary settings dialog.
-		self._myvocabularyChoice = vocabularySettingsGuiHelper.addLabeledControl(_("Select your Vocabulary:"), wx.Choice, choices=LIST_VOCS)
+		self._myvocabulary = _("Select your vocabulary:")
+
+		dialogSizer = wx.BoxSizer (wx.VERTICAL)
+		myvocabularyLabel = wx.StaticText (self, label = self._myvocabulary)
+		dialogSizer.Add (myvocabularyLabel)
+		self._myvocabularyChoice = wx.Choice (self, choices=LIST_VOCS)
+		dialogSizer.Add (self._myvocabularyChoice)
 		self._myvocabularyChoice.SetStringSelection(config.conf["vocabulary"]["myvocabulary"])
 
 	def postInit(self):
 		self._myvocabularyChoice.SetFocus()
 
-	def onOk(self, evt):
+
+
+	def onSave(self):
 		config.conf["vocabulary"]["myvocabulary"]=self._myvocabularyChoice.GetStringSelection()
-		super(vocabularySettingsDialog, self).onOk(evt)
 
