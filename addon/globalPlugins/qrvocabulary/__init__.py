6926196a220a89363331ff46452f24ada3bbb4ad
chrislm <llajta2012@gmail.com>
2019-02-28 14:06:22 +0100

    Added clipboard features, v.0.5.3A

--- addon/globalPlugins/qrvocabulary/__init__.py
+++ addon/globalPlugins/qrvocabulary/__init__.py
@@ -1,44 +1,70 @@
 # -*- coding: UTF-8 -*-
 
-#Copyright (C)2017-2018
+#v. 0.5.3A Copyright (C)2017-2019 by Chris Leo <llajta2012ATgmail.com>
+# Quickly Reach Vocabulary add-on for NVDA SCREEN READER.
 # Released under GPL 2
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
 import addonHandler
-import globalPluginHandler
-import textInfos
-import NVDAObjects
 import api
-import ui
-import os
 import cPickle
 import config
-import wx
+import configobj
+import glob
+import globalPluginHandler
+import globalVars
+from globalCommands import SCRCAT_TOOLS, SCRCAT_CONFIG
 import gui
 from gui import SettingsPanel, NVDASettingsDialog, guiHelper
 from logHandler import log
+import NVDAObjects
+import os
 import scriptHandler
-import configobj
-#from cStringIO import StringIO
-import glob
-from globalCommands import SCRCAT_TOOLS
 from scriptHandler import script
+import textInfos
+import threading
+import tones
+import ui
+import wx
+""" Development code   for download and instal the vocabularies.
+from . import warning
+"""
 
 addonHandler.initTranslation()
-
+# paths:
 ADDON_DIR = os.path.dirname(__file__)
 VOCS_DIR = os.path.join(ADDON_DIR, "vocabulary")
 LIST_VOCS = [os.path.split(path)[-1] for path in glob.glob(os.path.join(VOCS_DIR, '*.pkl'))]
-
+# configuration:
 confspec = {
-	'myvocabulary': 'string(default="esp.pkl")',
+	'myvocabulary': 'integer(default=0)',
 }
 config.conf.spec["vocabulary"] = confspec
+# Dictionaries for vocabulary names, iteritems using "six" library.
+vocabularies = {
+_("Spanish Vocabulary"): "esp.pkl",
+_("Italian Vocabulary"): "ita.pkl",
+"default": "_temp.dat"
+}
+import six
+tVocabularies = {v : k for k, v in six.iteritems(vocabularies)}
 
+# To remember the last results found.
+memo = ""
 
+def getVocsFile():
+	return [os.path.split(path)[-1] for path in glob.glob(os.path.join(VOCS_DIR, '*.pkl'))]
+
+def getVocsName():
+	vocsfiles = getVocsFile()
+	vf = tVocabularies.keys()
+	return [tVocabularies[vf] for vf in vocsfiles]
+
+# *lemmas* is the pikled dict   name for each vocabulary
 def loadVocabulary():
-	vocName = config.conf["vocabulary"]["myvocabulary"]
+	vocsfiles = getVocsFile()
+	vocName = vocsfiles[config.conf["vocabulary"]["myvocabulary"]]
 	try:
 		output = open(os.path.join(VOCS_DIR, vocName), "rb")
 		lemmas = cPickle.load(output)
@@ -50,20 +76,34 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def __init__(self):
 		super(globalPluginHandler.GlobalPlugin, self).__init__()
+
+		""" Development menu for download and instal the vocabularies.
+		self.toolsMenu = gui.mainFrame.sysTrayIcon.toolsMenu
+		self.vocabulary = self.toolsMenu.Append(wx.ID_ANY, _("Download Vocabularies."), _("Download Vocabularies..."))
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, warning.notice, self.vocabulary)
+		"""
+
 		NVDASettingsDialog.categoryClasses.append(vocabularySettingsPanel)
 
 	def terminate(self):
 		NVDASettingsDialog.categoryClasses.remove(vocabularySettingsPanel)
 
+		""" Development menu:
+		try:
+			self.toolsMenu.Remove(self.vocabulary)
+		except: #(RuntimeError, AttributeError, wx.PyDeadObjectError):
+			pass
+		"""
 
+# script to announce the meaning of selected word:
 	@script(
 		# Translators: Message presented in input help mode.
-		description=_("Announces the meaning of selected word."),
+		description=_("Announces the meaning of selected word found in the vocabulary."),
 		category = SCRCAT_TOOLS,
 		gesture="kb:NVDA+shift+f7"
 	)
 
-	def script_findMeaning(self, gesture):
+	def script_fromWordSelected(self, gesture):
 		obj=api.getFocusObject()
 		treeInterceptor=obj.treeInterceptor
 		if hasattr(treeInterceptor,'TextInfo') and not treeInterceptor.passThrough:
@@ -78,15 +118,70 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		else:
 			str = info.text
 			kwrd = str.lower().strip()
+			threading.Thread(target=self.findMeaning, args=(kwrd,)).start()
+
+	# script for clipboard searching
+	@script(
+		# Translators: Message presented in input help mode.
+		description=_("Search in the vocabulary the meaning of the word on clipboard."),
+		category = SCRCAT_TOOLS,
+		gesture="kb:NVDA+shift+f8"
+	)
+	def script_fromClip(self, gesture):
+		try:
+			text = api.getClipData()
+		except TypeError:
+			text = None
+		if not text or not isinstance(text,basestring):
+			#translators: message spoken when the clipboard is empty
+			ui.message(_("There is no text on the clipboard"))
+		else:
+			threading.Thread(target=self.findMeaning, args=(text,)).start()
+
+	# Script to copy on clipboard.
+	@script(
+		# Translators: Message presented in input help mode.
+		description=_("Copies on clipboard the last meaning found in the vocabulary."),
+		gesture="kb:NVDA+shift+f9",
+		category = SCRCAT_TOOLS
+	)
+	def script_copyClipboard(self, gesture):
+		if memo:
+			m=scriptHandler.getLastScriptRepeatCount()
+			if m==0:
+				api.copyToClip(memo)
+				tones.beep(540, 220)
+				ui.message(_("Copied."))
+		else:
+			#translators: message when  there are no recent searches in the vocabulary
+			ui.message(_("There is no recent research in the vocabulary."))
+
+	def findMeaning(self, kwrd):
+		global memo
+		if not os.path.exists(VOCS_DIR):
+			#from . import warning
+			#wx.CallLater(10, warning.notice)
+			ui.message(_("No vocabularies Installed!"))
+		else:
 			lemmas =loadVocabulary() 
-			if lemmas.has_key(kwrd):
-				ui.message("%s" %lemmas.get(kwrd))
+			if kwrd in lemmas.keys():
+				memo=(_('Found "{thisword}": %s').format(thisword = kwrd) %lemmas.get(kwrd))
+				ui.message(memo)
 			else:
 				# Translator: message when the searched word is not present in the dictionary.
-				ui.message(_("no match found for this word... "))
+				ui.message(_("Sorry, no match found for {thisword}. Selected vocabulary: {vocs}.").format(thisword = kwrd, vocs = getVocsName()[config.conf["vocabulary"]["myvocabulary"]]))
 
-	def onVocabularySettingsPanel(self, evt):
-		gui.mainFrame._popupSettingsDialog(NVDASettingsDialog, VocabularySettingsPanel)
+	def onSettings(self, evt):
+		gui.mainFrame._popupSettingsDialog(NVDASettingsDialog, vocabularySettingsPanel)
+
+	# Script to open settings dialog.
+	@script(
+		# Translators: Message presented in input help mode.
+		description=_("Shows settings for available vocabularies."),
+		category = SCRCAT_CONFIG
+	)
+	def script_settings(self, gesture):
+		wx.CallAfter(self.onSettings, None)
 
 class vocabularySettingsPanel(SettingsPanel):
 	# Translators: Title of the vocabulary settings dialog.
@@ -98,15 +193,12 @@ class vocabularySettingsPanel(SettingsPanel):
 		dialogSizer = wx.BoxSizer (wx.VERTICAL)
 		myvocabularyLabel = wx.StaticText (self, label = self._myvocabulary)
 		dialogSizer.Add (myvocabularyLabel)
-		self._myvocabularyChoice = wx.Choice (self, choices=LIST_VOCS)
+		self._myvocabularyChoice = wx.Choice (self, choices=getVocsName())
 		dialogSizer.Add (self._myvocabularyChoice)
-		self._myvocabularyChoice.SetStringSelection(config.conf["vocabulary"]["myvocabulary"])
+		self._myvocabularyChoice.SetSelection(config.conf["vocabulary"]["myvocabulary"])
 
 	def postInit(self):
 		self._myvocabularyChoice.SetFocus()
 
-
-
 	def onSave(self):
-		config.conf["vocabulary"]["myvocabulary"]=self._myvocabularyChoice.GetStringSelection()
-
+		config.conf["vocabulary"]["myvocabulary"]=self._myvocabularyChoice.GetSelection()
--- readme.md
+++ readme.md
@@ -1,16 +1,27 @@
 # Quickly Reach Vocabulary #
 
 * Authors: Christian Leo llajta2012ATgmail.com
+* NVDA compatibility: 2018.4 to 2019.1
 
 Allows you to quickly reach the meaning of a selected word.
 
 ##Usage:##
 
- From NVDA setting dialog  you can select   a vocabulary from Vocabulary settings category.
+ From NVDA setting dialog  you can select   a vocabulary below  Vocabulary settings category.
 
-Press NVDA+SHIFT+F7,  NVDA sayS the meaning of the selected word if available in the vocabulary.
+Select a word and press NVDA+SHIFT+F7: NVDA says the meaning of the selected word if available in the vocabulary.
 
-Available vocabularies:
+Other commands are:
+
+*	NVDA+SHIFT+F8: If there is a word on clipboard,  NVDA looks for the meaning of that  word;
+*	NVDA+SHIFT+F9: Copies last result on clipboard.
+*	Is also available one script to open the setting panel to choose your vocabulary.
+
+You can assign the key to the script under the configuration category, and under tools category for change your personal gestures.
+
+
+##Available vocabularies:##
+
 *	Spanish vocabulary;
 *	Italian vocabulary.
 
